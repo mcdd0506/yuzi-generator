4c606c18268383b40bb407cb020fee6fefbfee03
mcdd0506 <mcdd0506@gmail.com>
2024-08-15 15:21:51 +0800

    :sparkles: feat: generate picocli by freemarker

--- yuzi-generator-maker/src/main/java/mcdd/cbq/maker/App.java
+++ yuzi-generator-maker/src/main/java/mcdd/cbq/maker/App.java
@@ -1,10 +1,5 @@
 package mcdd.cbq.maker;
 
-import mcdd.cbq.maker.cli.CommandExecutor;
-import mcdd.cbq.maker.generator.file.StaticFileGenerator;
-
-import java.io.File;
-
 /**
  * Hello world!
  */

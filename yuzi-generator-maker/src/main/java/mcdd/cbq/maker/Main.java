b69b2887e006466d8926549a769a8d9443658c2c
mcdd0506 <mcdd0506@gmail.com>
2024-08-15 19:56:03 +0800

    :bug: fix: App --> Main

--- yuzi-generator-maker/src/main/java/mcdd/cbq/maker/Main.java
+++ yuzi-generator-maker/src/main/java/mcdd/cbq/maker/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
         MainGenerator.main(args);
     }
